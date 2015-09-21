a88a87181f55ab12660fdefbcec6ecb35269aaca
Yequ Sun <yequsun@gmail.com>
2015-09-21 08:15:04 -0400

    Submit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text counter_text;
+	public Text win_text;
+	private Rigidbody rb;
+	private int counter;
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		counter = 0;
+		win_text.text = "";
+		SetCounter ();
+	}
+
+
+	// Use this for initialization
+	void FixedUpdate (){
+		float moveHori = Input.GetAxis ("Horizontal");
+		float moveVerti = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHori, 0.0f, moveVerti);
+		SetCounter ();
+		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("pickup")) {
+			other.gameObject.SetActive (false);
+			counter++;
+		}
+
+	}
+
+	void SetCounter(){
+		counter_text.text = "Count: " + counter.ToString();
+		if (counter >= 15) {
+			win_text.text = "You Win!";
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	private Vector3 rotatevec = new Vector3 (15, 30, 45);
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotatevec*Time.deltaTime);
+	
+	}
+}
